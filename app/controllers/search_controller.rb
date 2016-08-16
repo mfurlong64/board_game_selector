05c09d0c118708b5412598fd428258751a044557
Michael Furlong <furthing45@Michaels-MacBook-Pro-4.local>
2016-08-16 08:40:32 -0700

    add functioning search which parses a users bgg collection

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -9,10 +9,23 @@ class SearchController < ApplicationController
 
 	def create
 		@search = params[:search]
-		@doc = Nokogiri::XML(open("https://boardgamegeek.com/xmlapi2/collection?username="+@search[:bgg_username]+"&own=1")) 
+		@doc = Nokogiri::XML(open("https://boardgamegeek.com/xmlapi2/collection?username="+@search[:bgg_username]+"&own=1"+"&stats=1")) 
 		
+		# apply_args(@doc)
+
 		render :show
 	end	
 
+	# def apply_args(docu)
+	# 	collection = {}
+	# 	@doc.css('item').each_with_index do |game, i|
+
+	# 		collection[i] = []
+	# 		if 
+	# 			collection[i] << game.at('name').text
+			
+	# 	end
+
+	# end
 
 end
--- app/helpers/search_helper.rb
+++ app/helpers/search_helper.rb
@@ -1,2 +1,39 @@
 module SearchHelper
+
+	def get_length
+		@length = @search[:game_length]
+		if @length == "Short"
+			return [0,44]
+		elsif @length == "Medium"
+			return [45,119]
+		elsif @length == "Long"
+			return [120,100000]
+		else
+			return [0,100000]
+		end
+	end
+
+	def get_weight
+		@weight = @search[:complexity]
+		if @weight == "Light"
+			return [0,2.00]
+		elsif @weight == "Normal"
+			return [2.01,2.99]
+		elsif @weight == "Heavy"
+			return [3.00,5.00]
+		else
+			return [0,5.00]
+		end
+	end
+
+	def get_players
+		@players = @search[:number_of_players].to_i
+	end
+
+	def get_top
+		doc = Nokogiri::HTML(open("https://boardgamegeek.com/browse/boardgame")) do |id|
+			id.strict.nonet
+		end
+	end
+
 end
